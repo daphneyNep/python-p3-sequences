0152c3a9c18b7fc110db81c151b0fe920ad71541
Daphney <daphneyalexandre@hotmail.com>
2024-06-26 09:22:17 -0400

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+import ipdb
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+        return
+    elif length == 1:
+        print('[0]')
+        return
+    elif length == 2:
+        print('[0, 1]')
+        return
+    else:
+        fib_sequence = [0, 1]
+        for _ in range(2, length):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence [-2])
+        print(fib_sequence)
+        
